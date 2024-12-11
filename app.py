64a9071a09d7308a240fb800dafb8d958e177229
yasmine <yasmine.elhakem@insat.ucar.tn>
2024-12-11 17:58:07 +0100

    socketio

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+@app.route('/')
+def index():
+    print(f"Templates folder path: {os.path.abspath(app.template_folder)}")  # Debugging line
+    return render_template('index.html')
+
+@socketio.on('my event')
+def handle_my_event(json):
+    print('Received event: ' + str(json))
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
